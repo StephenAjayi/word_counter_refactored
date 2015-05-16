a3911ca5c014afc65484fe17bf36c4ffc00d9f4c
stephen <lonelytreesl@yahoo.com>
2015-05-16 23:12:02 +0000

    add spec and code to # word_counter() to remove special characters from user input string, all specs passing.

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -1,8 +1,26 @@
+
+
 class String
   define_method(:word_counter) do |word_to_count|
-    words = split(" ")
+    letters = split("")
     counter = 0
+    alphabet = *("a".."z")
+    space = " "
+    only_letters_and_spaces = []
+    words = []
     
+    letters.each do |letter|
+
+      if alphabet.include?(letter)
+        only_letters_and_spaces.push(letter)
+      elsif letter.==(space)
+binding.pry
+      only_letters_and_spaces.push(letter)
+      end
+    end
+    
+    only_letters_and_spaces = only_letters_and_spaces.join("")
+    words = only_letters_and_spaces.split(" ")
     words.each() do |word|
       if word_to_count.==(word)
       counter = counter.+(1)
--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -1,12 +1,17 @@
 require('rspec')
-require('pry')
 require('word_counter')
+require('pry')
 
 describe('String#word_counter') do
   it('returns the right number of times a word appears') do
     expect("happy happy happy".word_counter("happy")).to(eq(3))
   end
+  
   it('returns 0 when the word to count does not appear') do
     expect('happy happy happy'.word_counter("movie")).to(eq(0))
+  end
+  
+  it('removes any characters other than letters and spaces from string') do
+    expect("happy,*&%$ happy !!!!".word_counter("happy")).to(eq(2))
   end
 end
